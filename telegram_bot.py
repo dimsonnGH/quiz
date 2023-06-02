fc65cd0e1bfaab561479a30cc9d3b8e89513ba99
dimsonnGH <dimsonn@mail.ru>
2023-06-03 02:02:52 +0400

    add telegram echo bot

---
+++ telegram_bot.py
@@ -0,0 +1,48 @@
+import logging
+import os
+from dotenv import load_dotenv
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+logger = logging.getLogger(__name__)
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    '''user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )'''
+    update.message.reply_text('Здравствуйте!')
+
+
+def main() -> None:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    load_dotenv()
+
+    telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    updater = Updater(telegram_token, use_context=True)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+
+    logger.info('Telegram bot started')
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
